2071c1e0fd40830f1ffc0ec330cf487932d915b9
raihanhykl <raihanhaikal40@gmail.com>
2024-11-21 14:19:27 +0700

    .

--- src/app/edit-application/components/progress.tsx
+++ src/app/edit-application/components/progress.tsx
@@ -44,7 +44,6 @@ const steps = [
 ];
 
 export default function ProgressStepper() {
-  //   const pathname = "/edit-application/alamat";
   const pathname = usePathname();
 
   const [containerWidth, setContainerWidth] = useState(0);
@@ -73,7 +72,6 @@ export default function ProgressStepper() {
     <div className="w-full bg-white" ref={containerRef}>
       <div className="relative px-4 py-8">
         <div className="relative flex items-start justify-between">
-          {/* Progress Line */}
           <div className="absolute left-0 top-[15px] h-[2px] w-full bg-gray-200">
             <div
               className="h-full bg-[#1e4d78] transition-all duration-300"
@@ -83,8 +81,6 @@ export default function ProgressStepper() {
               }}
             />
           </div>
-
-          {/* Steps */}
           {steps.map((step, index) => (
             <div
               key={step.id}
@@ -92,7 +88,6 @@ export default function ProgressStepper() {
               style={{ width: `${100 / steps.length}%` }}
             >
               <div
-                // href={step.path}
                 className={cn(
                   "relative z-10 flex h-6 w-6 sm:h-8 sm:w-8 items-center justify-center rounded-full border-2 text-xs sm:text-sm font-semibold",
                   index === currentStepIndex
